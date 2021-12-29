8d116237ad28a28bdefdfa2633ca994f3f7f8f2c
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-12-29 14:12:48 +0900

    [INIT] Project Inital

---
+++ src/main/java/com/nerd/need_server/NeedServerApplication.java
@@ -0,0 +1,13 @@
+package com.nerd.need_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeedServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nerd/need_server/NeedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerd.need_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeedServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
