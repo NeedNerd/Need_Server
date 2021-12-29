8fc88a78b21c8d4ab0756486a2c3f2814070e9bb
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-12-29 16:35:00 +0900

    [ADD] Base Response

---
+++ src/main/java/com/nerd/need_server/response/BaseResponse.java
@@ -0,0 +1,18 @@
+package com.nerd.need_server.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BaseResponse<T> {
+
+    private String message;
+    private int code;
+    private T data;
+
+}
