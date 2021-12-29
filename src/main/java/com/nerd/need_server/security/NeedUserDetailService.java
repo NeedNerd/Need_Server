66721321c291d44a9f630769ab2741e682075a77
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-12-29 16:33:24 +0900

    [ADD] Security & JWT

---
+++ src/main/java/com/nerd/need_server/security/NeedUserDetailService.java
@@ -0,0 +1,29 @@
+package com.nerd.need_server.security;
+
+import com.nerd.need_server.model.User;
+import com.nerd.need_server.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class NeedUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public NeedUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.getById(username);
+
+        return new org.springframework.security.core.userdetails.User(user.getId(), user.getPassword(), Collections.emptyList());
+    }
+}
