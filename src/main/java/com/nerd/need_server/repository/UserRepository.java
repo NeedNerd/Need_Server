8bf5af4a0d06a964200e8b6b786cfdcbcefa488c
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-12-29 22:30:36 +0900

    [FIX] User

--- src/main/java/com/nerd/need_server/repository/UserRepository.java
+++ src/main/java/com/nerd/need_server/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.nerd.need_server.repository;
 
 import com.nerd.need_server.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
+    public User getUserById(String id);
 
 }
